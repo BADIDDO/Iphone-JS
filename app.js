b6d14040f02258405824c0d6b651f09142c5525f
BADIDDO <aymenmatrix311@gmail.com>
2022-04-17 23:10:06 +0200

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+let imgs = document.querySelector(".img img");
+let container = document.querySelector("body");
+
+function phones(phone) {
+    imgs.src = phone;
+}
+
+function colors(color){
+container.style.background = color;
+}
